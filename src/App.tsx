31037ef76ff7ef2e171bc52afcd31f0cf9237073
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-12-08 23:25:41 +0530

    Checkbox handling to update corresponding states fn. added

--- src/App.tsx
+++ src/App.tsx
@@ -83,28 +83,48 @@ function LengthSlider() {
   );
 }
 
-const complexityOptions: string[] = [
-  "Include Uppercase Letters",
-  "Include Lowercase Letters",
-  "Include Numbers",
-  "Include Symbols",
+const complexityOptions = [
+  { id: "uppercase", name: "Include Uppercase Letters" },
+  { id: "lowercase", name: "Include Lowercase Letters" },
+  { id: "number", name: "Include Numbers" },
+  { id: "symbol", name: "Include Symbols" },
 ];
 
 function ComplexityOptions() {
+  const [isUppercaseChecked, setIsUppercaseChecked] = useState(false);
+  const [isLowercaseChecked, setIsLowercaseChecked] = useState(false);
+  const [isNumberChecked, setIsNumberChecked] = useState(false);
+  const [isSymbolChecked, setIsSymbolsChecked] = useState(false);
+
+  const handleCheckboxSelection = (e, optionId) => {
+    const value = e.target.checked;
+
+    if (optionId === "uppercase") {
+      setIsUppercaseChecked(value);
+    } else if (optionId === "lowercase") {
+      setIsLowercaseChecked(value);
+    } else if (optionId === "number") {
+      setIsNumberChecked(value);
+    } else {
+      setIsSymbolsChecked(value);
+    }
+  };
+
   return (
     <>
       <li className="list-none flex flex-col gap-2">
         {complexityOptions.map((option) => {
           return (
             <>
-              <div className="flex gap-4 items-center">
+              <div key={option.id} className="flex gap-4 items-center">
                 <input
                   type="checkbox"
-                  name="uppercase"
-                  id="uppercase"
+                  name={option.name}
+                  id={option.id}
+                  onChange={(e) => handleCheckboxSelection(e, option.id)}
                   className="accent-neon-green h-5 w-5"
                 />
-                <ul>{option}</ul>
+                <ul>{option.name}</ul>
               </div>
             </>
           );
